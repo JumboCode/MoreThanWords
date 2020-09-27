3b104ff8fdbe55b3c834dc68a0473d1f062d95c7
Titapa (PunPun) Chaiyakiturajai <titapapunne@gmail.com>
2020-09-27 12:38:47 -0400

    added prod and staging urls

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -3,5 +3,7 @@ export default {
   version: '1.0.0',
   extra: {
     apiUrl: 'http://YOUR.LOCAL.IP.ADDRESS:5000/'
+    // apiUrl: 'https://more-than-words.herokuapp.com/' // PROD url
+    // apiUrl: 'https://more-than-words-staging.herokuapp.com/' // STAGING url
   },
 };
