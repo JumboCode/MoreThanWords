0a0826e77e7561095bec8260420a8e031732d641
Jacqueline Chin <chin.jacqueline1@gmail.com>
2020-10-03 12:47:19 -0400

    MTW-01: added hello to app.js

--- frontend/App.js
+++ frontend/App.js
@@ -10,7 +10,7 @@ export default function App() {
     });
     return (
         <View style={styles.container}>
-            <Text>Open up App.js to start working on your app!</Text>
+            <Text>Open up App.js to start working on your app!Hello</Text>
             <Text>{data}</Text>
             <StatusBar style="auto" />
         </View>
