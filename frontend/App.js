16b2c58ac01dd759c015c3a3b8563f5acec5236f
soph-yang <thesophiayang2001@gmail.com>
2020-10-13 21:21:59 -0400

    Created touchable opacity buttons for each focus area

--- frontend/App.js
+++ frontend/App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import Constants from 'expo-constants';
 
 export default function App() {
@@ -8,21 +8,71 @@ export default function App() {
     fetch(Constants.manifest.extra.apiUrl).then(rawResult => rawResult.json()).then(jsonResult => {
         setData(jsonResult.data)
     });
+    
     return (
         <View style={styles.container}>
-            <Text>Open up App.js to start working on your app!</Text>
-            <Text>{data}</Text>
+            <Text style={styles.title}>
+                ASSOCIATE POD
+            </Text>
+            
+            <TouchableOpacity style={styles.block}>
+                <Text style={styles.blockText}>
+                    Competencies
+                </Text>
+                
+                <Text>Outcomes Achieved</Text>
+            </TouchableOpacity>
+            
+            <TouchableOpacity style={styles.block}>
+                <Text style={styles.blockText}>
+                    Career Pathway
+                </Text>
+                <Text>Outcomes Achieved</Text>
+            </TouchableOpacity>
+        
+            <TouchableOpacity style={styles.block}>
+                <Text style={styles.blockText}>
+                    Life Essentials/ Support Network
+                </Text>
+                <Text>Outcomes Achieved</Text>
+            </TouchableOpacity>
+                                    
             <StatusBar style="auto" />
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+        container: {
+                flex: 1,
+                backgroundColor: '#fff',
+                alignItems: 'center',
+                justifyContent: 'center',
+        },
+        title: {
+                fontSize: 30,
+                marginTop: 20,
+                fontWeight: 'bold',
+        },
+        block: {
+                marginTop: 40,
+                width: 300,
+                height: 180,
+                backgroundColor: '#fae484',
+                borderColor: 'white',
+                borderWidth: 1,
+                borderRadius: 10,
+                alignItems: 'center',
+                justifyContent: 'center',
+        },
+        blockText: {
+                fontSize: 25,
+                textAlign: 'center',
+        },
 });
 
+//questions: style and indenting? sizing? what do buttons do when pressed?
+//<Text>{data}</Text>
+//need help on states
+//the code for each pod component is so similar. is there a way to make it more modular? 
+//how can i do navigation if i don't have homescreen yet
