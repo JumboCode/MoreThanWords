405304f1dc5f3fce788428a904d669b7526e3f43
ehu01 <emilyh1700@gmail.com>
2020-10-17 11:21:08 -0400

    created basic component for home screen

---
+++ frontend/components/home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+class Home extends Component {
+    render() {
+        return (
+            <View>
+                <Text>
+                    hello world
+                </Text>
+            </View>
+        )
+    }
+}
+
+module.exports = Home;
