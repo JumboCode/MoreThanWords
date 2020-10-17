a140fbe931df8d6165438d6fe5788b08e65a4f87
soph-yang <thesophiayang2001@gmail.com>
2020-10-17 11:26:24 -0400

    Added test pod components to start trying to implement navigation/routing from a test home screen

--- frontend/App.js
+++ frontend/App.js
@@ -3,6 +3,9 @@ import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import Constants from 'expo-constants';
 
+import TestHomeScreen from './pod_components/TestHomeScreen.js';
+// import PodScreen from './pod_components/PodScreen.js';
+
 export default function App() {
     const [data, setData] = React.useState("default");
     fetch(Constants.manifest.extra.apiUrl).then(rawResult => rawResult.json()).then(jsonResult => {
@@ -10,35 +13,7 @@ export default function App() {
     });
     
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>
-                ASSOCIATE POD
-            </Text>
-            
-            <TouchableOpacity style={styles.block}>
-                <Text style={styles.blockText}>
-                    Competencies
-                </Text>
-                
-                <Text>Outcomes Achieved</Text>
-            </TouchableOpacity>
-            
-            <TouchableOpacity style={styles.block}>
-                <Text style={styles.blockText}>
-                    Career Pathway
-                </Text>
-                <Text>Outcomes Achieved</Text>
-            </TouchableOpacity>
-        
-            <TouchableOpacity style={styles.block}>
-                <Text style={styles.blockText}>
-                    Life Essentials/ Support Network
-                </Text>
-                <Text>Outcomes Achieved</Text>
-            </TouchableOpacity>
-                                    
-            <StatusBar style="auto" />
-        </View>
+        <TestHomeScreen />
     );
 }
 
@@ -48,30 +23,10 @@ const styles = StyleSheet.create({
                 backgroundColor: '#fff',
                 alignItems: 'center',
                 justifyContent: 'center',
-        },
-        title: {
-                fontSize: 30,
-                marginTop: 20,
-                fontWeight: 'bold',
-        },
-        block: {
-                marginTop: 40,
-                width: 300,
-                height: 180,
-                backgroundColor: '#fae484',
-                borderColor: 'white',
-                borderWidth: 1,
-                borderRadius: 10,
-                alignItems: 'center',
-                justifyContent: 'center',
-        },
-        blockText: {
-                fontSize: 25,
-                textAlign: 'center',
-        },
+        }
 });
 
-//questions: style and indenting? sizing? what do buttons do when pressed?
+//questions: style and indenting? sizing? style of buttons when pressed?
 //<Text>{data}</Text>
 //need help on states
 //the code for each pod component is so similar. is there a way to make it more modular? 
