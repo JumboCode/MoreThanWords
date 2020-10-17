5666995a865ddaa3550afa354fe1f50c5731adef
ehu01 <emilyh1700@gmail.com>
2020-10-17 11:51:27 -0400

    import and run Home component from App.js

--- frontend/App.js
+++ frontend/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Constants from 'expo-constants';
 
+import Home from './components/home'
+
 export default function App() {
     const [data, setData] = React.useState("default");
     fetch(Constants.manifest.extra.apiUrl).then(rawResult => rawResult.json()).then(jsonResult => {
@@ -13,6 +15,7 @@ export default function App() {
             <Text>Open up App.js to start working on your app!</Text>
             <Text>{data}</Text>
             <StatusBar style="auto" />
+            <Home />
         </View>
     );
 }
