7eb32d64af0262d9afe5b28581fd45a7522db8e8
waymao <my.waymao+git@gmail.com>
2020-10-25 01:01:47 +0800

    able to check if user exists in the salesforce db.

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,20 @@
 from flask import Flask
 from flask_cors import CORS
+from flask import request
+
+#from auth import requires_auth, AuthError
+from simple_salesforce import Salesforce, format_soql
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
+
+sf = Salesforce(username=os.getenv('SALESFORCE_USERNAME'),
+    password=os.getenv('SALESFORCE_PASSWORD'), 
+    security_token=os.getenv('SALESFORCE_SECURITY_TOKEN'))
 
 @app.route("/")
 def helloWorld():
@@ -10,6 +22,35 @@ def helloWorld():
     d['data'] = 'hello world!!!! pls work!!'
     d['status'] = 'good'
     return d
+
+
+@app.route("/reset", methods=['POST'])
+def resetPassword():
+    req_data = request.json
+    if 'email' not in req_data:
+        return {'data': 'no email present in the data',
+            'status': 'bad'}
+    
+    email = req_data['email']
+
+    print(email)
+
+    soql_str = """
+    SELECT count() FROM Contact WHERE MTW_Role__c = 'MTW Young Adult' AND Email = {email_value}
+    """
+    query_soql = format_soql(soql_str, email_value=email)
+    result = sf.query(query_soql)
+
+    print(result["totalSize"] >= 1)
+
+    # look up if email exists
+    if result["totalSize"] >= 1:
+        return {'data': 'Your verification email has been successly sent',
+                'status': 'good'}
+    else:
+        return {'data': 'Your Email is not registered in our databased.',
+                'status': 'bad'}
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
