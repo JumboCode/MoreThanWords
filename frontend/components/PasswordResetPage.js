89f5e64673c948f2e456c1fb8a78767f881acda9
waymao <my.waymao+git@gmail.com>
2020-10-27 00:58:17 +0800

    not-so-working auth0 email api request

--- frontend/components/PasswordResetPage.js
+++ frontend/components/PasswordResetPage.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button, Alert, Image } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Alert, Image, processColor } from 'react-native';
 import Constants from 'expo-constants';
 
 class PasswordResetPage extends React.Component {
@@ -15,13 +15,16 @@ class PasswordResetPage extends React.Component {
      * Alerts the user whether the request is successful or not.
      */
     send_email = async () => {
-        const request_body = {
-            "email": this.state.email
-        }
 
         try {
+            // CHECK IF EMAIL EXISTS in the SalesForce. Is it needed?
+            // TODO: Check if already signed up or not.
             // prepare and the request
-            const response = await fetch(`${Constants.manifest.extra.apiUrl}reset`, {
+            let request_body = {
+                "email": this.state.email
+            }
+
+            let response = await fetch(`${Constants.manifest.extra.apiUrl}reset`, {
                 method: 'POST',
                 body: JSON.stringify(request_body),
                 headers: {
@@ -29,18 +32,47 @@ class PasswordResetPage extends React.Component {
                 }
             });
 
-            const response_body = await response.json();
+            let response_body = await response.json();
 
             // gives alert based on response
             if (response.status != 200 || !('status' in response_body) || response_body.status !== 'good') {
                 Alert.alert("Sorry! your request failed.", response_body['data']);
-            } else {
-                Alert.alert("Success!", response_body['data']); 
+                return;
             }
 
         } catch (e) {
             console.log(e);
             Alert.alert("Sorry! your request failed.", "Network connection failed.");
+        }
+
+        try {
+            // SEND an email request to the Auth0 API.
+            // API Doc: https://auth0.com/docs/api/authentication
+            // TODO: process.env!!!
+            let request_body = {
+                "email": this.state.email,
+                "client_id": process.env.REACT_NATIVE_AUTH0_CLIENT_ID,
+                "connection": process.env.REACT_NATIVE_AUTH0_DBCONNECTION
+            };
+            console.log(request_body);
+
+            let response = await fetch(process.env.REACT_NATIVE_AUTH0_DOMAIN + "/dbconnections/change_password", {
+                method: 'POST',
+                body: JSON.stringify(request_body),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+
+            if (!response.ok) {
+                console.log(await response.json());
+                Alert.alert("Error requesting an email to be sent. Please try again soon.")
+            } else {
+                Alert.alert("Success!", await response.body()); 
+            }
+        } catch (e) {
+            console.log(e);
+            Alert.alert("Error requesting an email to be sent. Please try again soon.")
         }
     }
 
@@ -69,10 +101,11 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: 50
   }, logo: {
-      width: 100,
-      height: 50
+      width: 300,
+      height: 150
   }
 });
 
