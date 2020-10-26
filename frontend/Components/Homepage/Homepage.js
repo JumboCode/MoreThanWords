78991b19e9ec388b661d64930b17f4aa64c4e766
Alec Xu <alecxu@Alecs-MacBook-Pro-2.local>
2020-10-26 01:23:35 -0400

    fixed state

--- frontend/Components/Homepage/Homepage.js
+++ frontend/Components/Homepage/Homepage.js
@@ -1,27 +1,49 @@
 import React, { useState, } from 'react'
-import { StyleSheet, Text, View, Image, TextInput, Button } from 'react-native';
+import { StyleSheet, 
+        Text, 
+        View, 
+        Image, 
+        TextInput, 
+        Button } from 'react-native';
 
 export default function Homepage() {
     const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    function handleEmailInput(e) {
+        setEmail(e)
+    }
+
+    function handlePassInput(e) {
+        setPassword(e)
+    }
+
     return (
         <View style={styles.container}>
             <Image 
                 style={styles.image}
                 source={require('./TransparentMTWLogo.png')}
             />
+            <View style={styles.inputContainer}>
                 <TextInput 
+                    onChangeText={(value) => handleEmailInput(value)}
                     style={styles.inputField}
                     placeholder={'Email'}    
                 />
                 <TextInput 
+                    // secureTextEntry
                     style={styles.inputField}    
+                    onChangeText={(value) => handlePassInput(value)}
                     placeholder={'Password'}
                 />
-            <Button 
-                title="Login"
-                color="red"
-                accessibilityLabel="Login"
-            />
+            </View>
+            <View style={styles.button}>
+                <Button 
+                    title="LOGIN"
+                    color="#FFFFFF"
+                    accessibilityLabel="Login"
+                />
+            </View>
         </View>
     )
 }
@@ -29,23 +51,37 @@ export default function Homepage() {
 const styles = StyleSheet.create({
     container: {
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+        position: 'absolute',
+        marginHorizontal: 16
     },
     image: {
-        marginBottom: '0%',
-        marginTop: "-50%"
+        marginBottom: '20%',
+        marginTop: "-50%",
+    },
+    inputContainer: {
+        position: 'relative',
+        width: '68%',
+        marginLeft: '16%',
+        marginRight: '16%'
     },
     inputField: {
-        position: 'relative',
-        height: '15%',
+        height: '80%',
         marginTop: '3%',
         backgroundColor: '#e0dcdc',
         paddingLeft: '10%',
         paddingRight: '40%',
-        borderRadius: 2,
+        borderRadius: 2, 
         alignItems: 'center',
     },
-    login: {
-        color: 'red',
+    button: {
+        marginVertical: 3,
+        paddingLeft: '25%',
+        paddingRight: '25%',
+        paddingTop: '1.5%',
+        paddingBottom: '1.5%',
+        backgroundColor: '#FF3D3D',
+        marginTop: '25%',
+        borderRadius: 5,
     }
 })
