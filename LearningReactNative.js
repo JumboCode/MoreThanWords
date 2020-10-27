42cd41520d74c81be1909ef3e6dba9ae72807f45
soph-yang <thesophiayang2001@gmail.com>
2020-10-27 03:11:06 -0400

    19: clean up

--- LearningReactNative.js
+++ LearningReactNative.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity, Image, Button, 
 import Constants from 'expo-constants';
 
 // import TestHomeScreen from './pod_components/TestHomeScreen.js';
-import PodScreen from './pod_components/PodScreen.js';
+// import PodScreen from './pod_components/PodScreen.js';
 
 export default function App() {  
     const [data, setData] = React.useState("default");
@@ -13,54 +13,48 @@ export default function App() {
     });
         
     return (
-           <SafeAreaView style={styles.container}>    
-                <Text style={{fontSize: 30}}>{data}</Text>
-                <Text style={{fontSize: 50}} onPress={() => setData("efa default")}>HELLO WORLDD</Text>
-                <Text style={{fontSize: 30}}>{data}</Text>
-                
-                <Button 
-                    onPress={() => Alert.alert(
-                        "MY TITLE", 
-                        "wassup bro", 
-                        [
-                            {
-                                text: "Cancel", 
-                                style: "destructive", 
-                                onPress: () => console.log("cancel pressed")},
-                            { text: "No"},
-                            { text: "Yes"}
-                        ]
-                    )}
-                        color="green" 
-                        title="jumbocode"
-                />
-                
-                <TouchableOpacity onPress={() => Alert.prompt("title", "description", text => console.log(text))}>
-                        <Image 
-                            resizeMode="stretch"
-                            blurRadius={3}
-                            source={{
-                                height: 300,
-                                width: 200,
-                                uri: "https://picsum.photos/200/300"
-                            }} 
-                        />
-                </TouchableOpacity>
-           </SafeAreaView>
+    <SafeAreaView style={styles.container}>    
+        <Text style={{fontSize: 30}}>{data}</Text>
+        <Text style={{fontSize: 50}} onPress={() => setData("efa default")}>HELLO WORLDD</Text>
+        <Text style={{fontSize: 30}}>{data}</Text>
+        
+        <Button 
+            onPress={() => Alert.alert(
+                "MY TITLE", 
+                "wassup bro", 
+                [
+                    {
+                        text: "Cancel", 
+                        style: "destructive", 
+                        onPress: () => console.log("cancel pressed")},
+                    { text: "No"},
+                    { text: "Yes"}
+                ]
+            )}
+            color="green" 
+            title="jumbocode"
+        />
+        
+        <TouchableOpacity onPress={() => Alert.prompt("title", "description", text => console.log(text))}>
+            <Image 
+                resizeMode="stretch"
+                blurRadius={3}
+                source={{
+                    height: 300,
+                    width: 200,
+                    uri: "https://picsum.photos/200/300"
+                }} 
+            />
+        </TouchableOpacity>
+    </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
-        container: {
-                flex: 1,
-                backgroundColor: '#fff',
-                alignItems: 'center',
-                justifyContent: 'center',
-        }
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
 });
-
-//questions: style and indenting? sizing? style of buttons when pressed?
-//<Text>{data}</Text>
-//need help on states
-//the code for each pod component is so similar. is there a way to make it more modular? 
-//how can i do navigation if i don't have homescreen yet
