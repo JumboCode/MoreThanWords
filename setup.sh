8c5f48c99e9ebcf07934db6099e25a8befcf6507
Jacqueline Chin <chin.jacqueline1@gmail.com>
2020-10-28 13:14:09 -0400

    setup.sh chnaged back to original

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@
 git update-index --assume-unchanged frontend/app.config.js
 
 ## Run below if you want to make global changes to app.config.js
-# git update-index --no-assume-unchanged frontend/app.config.js
+## git update-index --no-assume-unchanged frontend/app.config.js
