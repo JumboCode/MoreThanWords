7e3706e6b378d538ca3d7e68c0fe944152636d50
ehu01 <emilyh1700@gmail.com>
2020-11-04 12:37:57 -0500

    merge App.js with staging

--- frontend/App.js
+++ frontend/App.js
@@ -2,11 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import Constants from 'expo-constants';
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-<<<<<<< HEAD
-import HomeScreen from './components/HomeScreen.js';
+import HomeScreen from './components/pod_components/HomeScreen.js';
 import TraineePodScreen from './components/pod_components/TraineePod.js';
 import AssociatePodScreen from './components/pod_components/AssociatePod.js';
 import PartnerPodScreen from './components/pod_components/PartnerPod.js';
@@ -24,29 +23,6 @@ export default function MainStackNavigator() {
                <Stack.Screen name="Random Screen" component={RandomScreen} />
             </Stack.Navigator>
         </NavigationContainer>
-=======
-import LotsOfPods from './components/home'
-import PodProgressBar from './components/ProgressBar'
-
-export default function App() {  
-    const [data, setData] = React.useState("default");
-    fetch(Constants.manifest.extra.apiUrl).then(rawResult => rawResult.json()).then(jsonResult => {
-        setData(jsonResult.data) 
-    });
-        
-    return (
-        <View> 
-         <View style={styles.container}>
-            {/* <Text>Open up App.js to start working on your app!</Text> */}
-             {/* <Text>{data}</Text> */}
-           <StatusBar style="auto" />
-         {/* Home pods */}
-             <LotsOfPods />
-             <PodProgressBar />
-        </View>
-
-       </View>
->>>>>>> cad77ccd45fa32669908eacb5e4f2a916415b01f
     );
 }
 
