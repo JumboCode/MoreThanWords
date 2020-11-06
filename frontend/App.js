9bbe4fc35960ccd9deeec45ba8071dd5458b7cf0
waymao <my.waymao+git@gmail.com>
2020-11-06 19:20:35 +0800

    removed unused component

--- frontend/App.js
+++ frontend/App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import Constants from 'expo-constants';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
