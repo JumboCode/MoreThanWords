6f7b0fd10c9cecd604d2df9590f38da99a0593a8
Alec Xu <alecxu@Alecs-MacBook-Pro-2.local>
2020-11-07 09:57:55 -0500

    add salesforce libraries

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,10 @@
 from flask import Flask
 from flask_cors import CORS
+import os
+from simple_salesforce import Salesforce, format_soql
+
+
+sf = Salesforce(username=os.environ['titapapunne@gmail.com'],password=os.environ['6Tj4DlqskZSb'],security_token=os.environ['ZXNQA0bvcVWJ0eiulugR9gBej'])
 
 app = Flask(__name__)
 CORS(app)
@@ -10,6 +15,10 @@ def helloWorld():
     d['data'] = 'hello world!!!! pls work!!'
     d['status'] = 'good'
     return d
+
+@app.route("/youthCheckbox")
+def youthCheck():
+    
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
