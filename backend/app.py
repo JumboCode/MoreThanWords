c72539ea89effca0eeb947455949b65f5a9e731a
waymao <my.waymao+git@gmail.com>
2020-11-14 23:11:39 +0800

    basic auth0 stuff

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,31 @@
 from flask import Flask
 from flask_cors import CORS
 import os # for environment variables
-from flask import request # imported for parsing arguemnts
+from flask import request, jsonify # imported for parsing arguemnts
 from simple_salesforce import Salesforce, format_soql # import Salesforce
+
+from auth import AuthError, requires_auth
+
 
 # global connection to Salesforce so we don't need to connect everytime, CHANGE TO ENV VARIABLE OR take  
 sf = Salesforce(username=os.environ['SALESFORCE_USERNAME'],password=os.environ['SALESFORCE_PASSWORD'],security_token=os.environ['SALESFORCE_SECURITY_TOKEN'])
 app = Flask(__name__)
 CORS(app)
+
+
+# Error handler for the Auth Error
+@app.errorhandler(AuthError)
+def handle_auth_error(ex):
+    response = jsonify(ex.error)
+    response.status_code = ex.status_code
+    return response
+
+
+@app.route("/auth-required")
+@requires_auth
+def sample():
+    return "OK"
+
 
 # route to verify sign up and check whether user who wants to register is 
 # allowed to use the app by checking salesforce database
