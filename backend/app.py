adf3c2b5ab9be5a9fa06372fa16fad0232153bad
Alec Xu <alecxu@Alecs-MacBook-Pro-2.local>
2020-11-21 10:18:41 -0500

    temp

--- backend/app.py
+++ backend/app.py
@@ -16,12 +16,16 @@ def helloWorld():
     field_names = [field['name'] for field in desc['fields']]
     soql = "SELECT {} FROM Trainee_POD_Map__c".format(','.join(field_names))
     result = sf.query(format_soql((soql + " WHERE Contact__c={youth_id}"), youth_id=Id))
-    return result
+    return desc
 
-# @app.route("/youthCheckbox")
-# def youthCheck():
-#     result = sf.query(format_soql("SELECT TR_Attendance__c FROM Trainee_POD_Map__c WHERE Contact__c={name_id}"), name_id=result.Id)
-#     return result
+@app.route("/youthCheckbox")
+def youthCheck():
+    # Id = "0030d00002VUcwSAAT"
+    # desc = sf.Trainee_POD_Map__c.describe()
+    # field_names = [field['name'] for field in desc['fields']]
+    # soql = "SELECT {} FROM Trainee_POD_Map__c".format(','.join(field_names))
+    # result = sf.query(format_soql((soql + " WHERE Contact__c={youth_id}"), youth_id=Id))
+    # return result
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
