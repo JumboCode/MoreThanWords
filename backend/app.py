16e78df4af3362cb312c3afad861a46922ad737d
Ryan Beckwith <ryanbeckwith2000@gmail.com>
2020-12-07 15:58:48 -0500

    Retrieved initial data from Salesforce

--- backend/app.py
+++ backend/app.py
@@ -5,9 +5,19 @@ from flask import request # imported for parsing arguemnts
 from simple_salesforce import Salesforce, format_soql # import Salesforce
 
 # global connection to Salesforce so we don't need to connect everytime, CHANGE TO ENV VARIABLE OR take  
-sf = Salesforce(username=os.environ['SALESFORCE_USERNAME'],password=os.environ['SALESFORCE_PASSWORD'],security_token=os.environ['SALESFORCE_SECURITY_TOKEN'])
+# sf = Salesforce(username=os.environ['SALESFORCE_USERNAME'],password=os.environ['SALESFORCE_PASSWORD'],security_token=os.environ['SALESFORCE_SECURITY_TOKEN'])
+sf = Salesforce(username="titapapunne@gmail.com",password="6Tj4DlqskZSb",security_token="ZXNQA0bvcVWJ0eiulugR9gBej")
 app = Flask(__name__)
 CORS(app)
+
+@app.route("/youthCheckbox")
+def youthCheck():
+    Id = "0030d00002VUcwSAAT"
+    desc = sf.Trainee_POD_Map__c.describe()
+    field_names = [field['name'] for field in desc['fields']]
+    soql = "SELECT {} FROM Trainee_POD_Map__c".format(','.join(field_names))
+    result = sf.query(format_soql((soql + " WHERE Contact__c={youth_id}"), youth_id=Id))
+    return result
 
 # route to verify sign up and check whether user who wants to register is 
 # allowed to use the app by checking salesforce database
--- frontend/components/outcomes/OutcomesScreen.js
+++ frontend/components/outcomes/OutcomesScreen.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView, FlatList } from 'react-native';
 import Outcome from './Outcome';
+import Constants from 'expo-constants';
 
 const styles = StyleSheet.create({
     container: {
@@ -14,7 +15,7 @@ const styles = StyleSheet.create({
 
 const titles = ["Example 1", "Example 2"];
 
-const data = titles.map((title) => {
+let data = titles.map((title) => {
     return {
         title: title,
         content: [
@@ -38,6 +39,64 @@ const data = titles.map((title) => {
 });
 
 export default function OutcomesScreen({ navigation }) {
+
+    const [dataTemp, setDataTemp] = useState(data);
+
+    useEffect(() => {
+            async function fetchData() {
+                let dataFromBackend;
+                // call endpoint
+                // await fetch(`${Constants.manifest.extra.apiUrl}/youthCheckbox`)
+                await fetch(`${Constants.manifest.extra.apiUrl}/youthCheckbox`)
+                    .then(response => response.json())
+                    .then(data => {
+                        // if not verified tell them
+                        console.log(data);
+                        dataFromBackend = data;
+                        let dataFields = dataFromBackend.records[0];
+                        let trDataFields = {};
+                        for (const key in dataFields) {
+                            console.log(key);
+                            if (key.includes("TR") && !key.includes("Outcome")) {
+                                trDataFields[key] = dataFields[key];
+                            }
+                        }
+                        // update data
+                        console.log("Hello");
+                        console.log(trDataFields);
+
+                        let counter = true;
+                        // Once outcome titles are known, replace this
+                        let newData = [{
+                            title: "Example",
+                            content: []
+                        }];
+                        for (const key in trDataFields) {
+                            if (counter) {
+                                // Must be a YDM attribute
+                                newData[0].content.push({
+                                    key: key,
+                                    starIsFilled: trDataFields[key],
+                                });
+                            } else {
+                                newData[0].content[newData[0].content.length - 1].checked = trDataFields[key];
+                            }
+                            counter = !counter;
+                        }
+                        console.log(newData);
+                        setDataTemp(newData);
+                    })
+                    .catch((error) => {
+                        console.log("An error occurred.");
+                        console.log(error);
+                    });
+                // parse returned data
+                
+                // setDataTemp(dataFromBackend);
+            }
+            fetchData();
+        }, []);
+
     return (
         <SafeAreaView style={styles.container}>
             {/* <Text>
@@ -45,7 +104,7 @@ export default function OutcomesScreen({ navigation }) {
             </Text> */}
             <FlatList 
                 style={styles.listStyle}
-                data={data}
+                data={dataTemp}
                 renderItem={({ item }) => {
                     return (
                         <Outcome data={[item]}/>
