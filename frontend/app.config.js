559cc83f64891526c47bf87dc3bab82a91d3effd
Titapa (PunPun) Chaiyakiturajai <titapapunne@gmail.com>
2021-02-07 20:23:37 -0500

    restore app.config.js

---
+++ frontend/app.config.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+export default {
+  name: 'MoreThanWords',
+  version: '1.0.0',
+  scheme: "morethanwords",
+  extra: {
+    apiUrl: 'http://YOUR.LOCAL.IP.ADDRESS:5000',
+    // apiUrl: 'https://more-than-words.herokuapp.com/' // PROD url
+    // apiUrl: 'https://more-than-words-staging.herokuapp.com/' // STAGING url
+    auth0_domain: process.env.AUTH0_DOMAIN,
+    auth0_client_id: process.env.AUTH0_CLIENT_ID,
+    auth0_dbconnection: process.env.AUTH0_DBCONNECTION,
+    api_audience: process.env.API_AUDIENCE
+  },
+};
