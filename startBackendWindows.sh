db38eeb9297bf85071316f6f0cdb1032a212feca
Ryan Beckwith <ryanbeckwith2000@gmail.com>
2021-02-13 13:25:10 -0500

    Fixed startBackendWindows.sh

--- startBackendWindows.sh
+++ startBackendWindows.sh
@@ -3,5 +3,4 @@ cd backend
 py -m venv backendEnv
 source backendEnv/scripts/activate
 pip install -r requirements.txt
-# eval $(egrep -v '^#' ../.env | xargs) py app.py
-py ~/OneDrive/Documents/Ryan/School/"College 2nd Year"/JumboCode/MoreThanWords/MoreThanWords/backend/app.py
+eval $(egrep -v '^#' ../.env | xargs) py app.py
