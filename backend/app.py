f5160292debc105333e3d1c7e004678a395e8a42
Irina Wang <irina.wang@outlook.com>
2021-02-24 15:37:43 -0500

    "remove debug print statement"

--- backend/app.py
+++ backend/app.py
@@ -52,7 +52,7 @@ def verify():
     # salesforce query based on the email, firstname & lastname
     result = sf.query(format_soql("SELECT Id, Email FROM Contact WHERE (email = {email_value} AND name={full_name})", email_value=email, full_name=name))
 
-    print(result["totalSize"] == 1)
+    # print(result["totalSize"] == 1)
 
     if (result["totalSize"] == 1):
         return {"verified": bool(1)} # true
