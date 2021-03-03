68714cc0f890a37fee285f9ad1e79690c1605082
Irina Wang <irina.wang@outlook.com>
2021-03-03 05:41:01 -0500

    deleted hardcode data; change css to reflect abe's design; fetch new endpoint name; add associate's data(waiting for partner pod)

--- frontend/components/pod_components/HomeScreen.js
+++ frontend/components/pod_components/HomeScreen.js
@@ -3,28 +3,22 @@ import React from 'react';
 import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity} from 'react-native';
 import Constants from 'expo-constants';
 import { getAccessToken } from '../../utils/auth.js';
-// import axios from 'axios';
-
 import ProgressBar from '../ProgressBar.js';
-import Axios from 'axios';
-
-const   TRAINEE_TOTAL_OUTCOMES = 7
-const ASSOCIATE_TOTAL_OUTCOMES = 11
-const   PARTNER_TOTAL_OUTCOMES = 9
 
 const server_add = Constants.manifest.extra.apiUrl;
 
 export default class HomeScreen extends React.Component{
-    
     state = {
         Trainee_progress: 0, 
+        Trainee_total: 0,
         Associate_progress: 0,
-        Partner_progress: 0
+        Associate_total: 0,
+        Partner_progress: 0,
+        Partner_total: 0,
     };
-
     componentDidMount(){
         getAccessToken().then(accessToken => 
-            fetch(server_add + '/calculateProgressBar', {
+            fetch(server_add + '/calcProgressHomeScreen', {
                 "headers": {
                     "Authorization": "Bearer " + accessToken
                 }
@@ -33,64 +27,64 @@ export default class HomeScreen extends React.Component{
             let data = await response.json();
             console.log(data);
             this.setState({
-                Trainee_progress: data.Trainee_sum,
-                // should change to *data.Associate_sum*
-                Associate_progress: 0,
-                // should change to *data.Partner_sum*
-                Partner_progress: 0
-                
+                Trainee_progress: data.Trainee_complete,
+                Trainee_total: data.Trainee_total,
+                Associate_progress: data.Associate_outcome,
+                Associate_total: data.Associate_total,
+                Partner_progress: 1, // should change to *data.Partner_sum* data.Associate_sum
+                Partner_total: data.Partner_total,
             })
         })
         .catch(function (error) {
             // TODO: error message 
-            // no progress to display, if you think it's an error
+            // no progress to display, 
             // component
             console.log(error);
         });
     }
 
     render(){
-        // Currently only have fake data, need to compare between real data
-        const iscompleted = true;
-        let displayBlock;
-        let displayBlockText;
-        let ProgressBarDisplay;
-        let ProgressBarText;
-        let ProgressBarFill;
-            if (iscompleted) {
-                displayBlock = styles.completedBlock;
-                displayBlockText = styles.completedBlockText;
-                ProgressBarDisplay = styles.completedBlockProgressBar;
-                ProgressBarDisplayText = styles.completedBlockProgressBarText;
-                ProgressBarFill = styles.completedBlockProgressBarFill;
-            } else{
-                displayBlock = styles.block;
-            }
+        let T_displayBlock = styles.Block;
+        let A_displayBlock = styles.Block;
+        let P_displayBlock = styles.Block;
+        let T_displayBlockText = styles.BlockText;
+        let A_displayBlockText = styles.BlockText;
+        let P_displayBlockText = styles.BlockText;
+        if (this.state.Trainee_progress < this.state.Trainee_total){ // highlight Trainee
+            T_displayBlock = styles.highlightBlock;
+            T_displayBlockText = styles.highlightBlockText;
+        } else if(this.state.Associate_progress < this.state.Associate_total){ // highlight Associate
+            A_displayBlock = styles.highlightBlock;
+            A_displayBlockText = styles.highlightBlockText;
+        } else { // highlight Partner
+            P_displayBlock = styles.highlightBlock;
+            P_displayBlockText = styles.highlightBlockText;
+        }
            
         return(
         <SafeAreaView style={styles.container}>
         <TouchableOpacity 
-            style={displayBlock} 
+            style={T_displayBlock} 
             onPress={() => this.props.navigation.navigate('Trainee Pod')}
         >
-                <Text style={displayBlockText}> Trainee </Text>
-            <ProgressBar style={ProgressBarDisplay} Textstyle={ProgressBarText} fill={ProgressBarFill} progress={this.state.Trainee_progress} total_outcomes={TRAINEE_TOTAL_OUTCOMES} />
+                <Text style={T_displayBlockText}> Trainee </Text>
+            <ProgressBar   progress={this.state.Trainee_progress} total_outcomes={this.state.Trainee_total} />
         </TouchableOpacity>
       
-        <TouchableOpacity 
-            style={displayBlock} 
+        <TouchableOpacity className="sidebar"
+            style={A_displayBlock} 
             onPress={() => this.props.navigation.navigate('Associate Pod')}
         >
-            <Text style={displayBlockText}> Associate </Text>
-            <ProgressBar style={ProgressBarDisplay} Textstyle={ProgressBarText} fill={ProgressBarFill} progress={this.state.Associate_progress} total_outcomes={ASSOCIATE_TOTAL_OUTCOMES} />
+            <Text style={A_displayBlockText}> Associate </Text>
+            <ProgressBar progress={this.state.Associate_progress} total_outcomes={this.state.Associate_total} />
         </TouchableOpacity>
     
         <TouchableOpacity 
-            style={displayBlock} 
+            style={P_displayBlock} 
             onPress={() => this.props.navigation.navigate('Partner Pod')}
         >                
-            <Text style={displayBlockText}> Partner </Text>
-            <ProgressBar style={ProgressBarDisplay} Textstyle={ProgressBarText} fill={ProgressBarFill} progress={this.state.Partner_progress} total_outcomes={PARTNER_TOTAL_OUTCOMES} />
+            <Text style={P_displayBlockText}> Partner </Text>
+            <ProgressBar  progress={this.state.Partner_progress} total_outcomes={this.state.Partner_total} />
         </TouchableOpacity>
     </SafeAreaView>);
     }
@@ -103,7 +97,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
-    completedBlock: {
+    Block: {
         marginTop: 20,
         width: '100%',
         height: 220,
@@ -114,33 +108,15 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
-    completedBlockText: {
+    BlockText: {
         fontSize: 40,
         // fontFamily: 'Roboto',
         color: '#27b48f',
         fontWeight: 'bold',
         textAlign: 'center',
     },
-    completedBlockProgressBar: {
-        flexDirection: 'row',
-        height: 20,
-        width: 250,
-        backgroundColor: 'white',
-        borderColor: '#27b48f', 
-        borderWidth: 2,
-        borderRadius: 10,
-    },
-    completedBlockProgressBarText: {
-        color: '#27b48f',
-        fontFamily: 'Roboto',
-    },
-    completedBlockProgressBarFill: {
-        color: '#27b48f',
-        fontFamily: 'Roboto',
-    },
-
     // ONGOING Block
-    ongoingBlock: {
+    highlightBlock: {
         marginTop: 20,
         width: '100%',
         height: 220,
@@ -151,31 +127,11 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
-    ongoingBlockText: {
+    highlightBlockText: {
         fontSize: 40,
-        fontFamily: 'Roboto',
+        // fontFamily: 'Roboto',
         color: '#ffffff',
         fontWeight: 'bold',
         textAlign: 'center',
     },
-
-    // TODO Block
-    todoBlock: {
-        marginTop: 20,
-        width: '100%',
-        height: 220,
-        backgroundColor: '#ececec',
-        borderColor: 'white',
-        borderWidth: 1,
-        borderRadius: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    todoBlockText: {
-        fontSize: 40,
-        fontFamily: 'Roboto',
-        color: '#9e9e9e',
-        fontWeight: 'bold',
-        textAlign: 'center',
-    },  
 });
