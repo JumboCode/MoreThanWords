e8f9ad71400b3bb8989849de2837b44a75ea9411
Jacqueline Chin <chin.jacqueline1@gmail.com>
2021-03-06 10:50:14 -0500

    removed header from login page

--- frontend/App.js
+++ frontend/App.js
@@ -74,7 +74,8 @@ class MainStackNavigator extends React.Component {
                             name="Login Screen"
                             component={componentWithRefreshFunc(LoginScreen, this.refreshLoginState)}
                             options={{
-                                animationEnabled: false,
+                                headerShown: false,
+                                animationEnabled: false
                             }}
                         />
                     )}
