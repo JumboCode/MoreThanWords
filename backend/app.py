657b0ab482ca46288cbfe4b52f34b38960b6b302
Jacqueline Chin <chin.jacqueline1@gmail.com>
2021-03-05 20:45:12 -0500

    removed extraneous print statement

--- backend/app.py
+++ backend/app.py
@@ -113,8 +113,6 @@ def verify():
     # salesforce query based on the email, firstname & lastname
     result = sf.query(format_soql("SELECT Id, Email FROM Contact WHERE (email = {email_value} AND name={full_name})", email_value=email, full_name=name))
 
-    # print(result["totalSize"] == 1)
-
     if (result["totalSize"] == 1):
         return {"verified": bool(1)} # true
 	
