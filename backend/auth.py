d09d078db92f64b37e04be78c1aa9a048f7524b2
waymao <my.waymao+git@gmail.com>
2021-03-14 01:13:34 +0800

    jwt validation

--- backend/auth.py
+++ backend/auth.py
@@ -1,16 +1,23 @@
+###############################################################################
 # auth.py
 # Part of: MTW web project
 # ref from
 # https://github.com/auth0-samples/auth0-python-api-samples/
-
+#
+# The main function of this file is requires_auth.
+#
+# It first gets the auth token by calling get_token_auth_header.
+# Then it moves to decode_and_verify_payload, which verifies the auth header.
+# Then it sends request to salesforce for user info in get_user_info.
+###############################################################################
 from functools import wraps
 import json
 from os import environ as env
-from six.moves.urllib.request import urlopen
 
 from dotenv import load_dotenv, find_dotenv
 from flask import Flask, request, _request_ctx_stack
 from flask_cors import cross_origin
+from jose import jwt
 
 from simple_salesforce import format_soql
 
@@ -27,13 +34,70 @@ AUTH0_DOMAIN = env.get("AUTH0_DOMAIN")
 API_IDENTIFIER = env.get("API_IDENTIFIER")
 ALGORITHMS = ["RS256"]
 
-AUTH_HEADER_PREFIX = "bearer"
+AUTH_HEADER_PREFIX = "jwt"
 
 # Format error response and append status code.
 class AuthError(Exception):
     def __init__(self, error, status_code):
         self.error = error
         self.status_code = status_code
+
+
+def decode_and_verify_payload(jwt_token):
+    """
+    decodes the token and verifies if it's issued by auth0.
+    if it's valid, the payload will be returned.
+    otherwise, an AuthError will be raised.
+    """
+    
+    request = requests.get(AUTH0_DOMAIN+"/.well-known/jwks.json")
+    jwks = request.json()
+    try:
+        unverified_header = jwt.get_unverified_header(jwt_token)
+    except jwt.JWTError:
+        raise AuthError({"code": "invalid_header",
+                        "description":
+                            "Invalid header. "
+                            "Use an RS256 signed JWT Access Token"}, 401)
+    if unverified_header["alg"] == "HS256":
+        raise AuthError({"code": "invalid_header",
+                        "description":
+                            "Invalid header. "
+                            "Use an RS256 signed JWT Access Token"}, 401)
+    rsa_key = {}
+    for key in jwks["keys"]:
+        if key["kid"] == unverified_header["kid"]:
+            rsa_key = {
+                "kty": key["kty"],
+                "kid": key["kid"],
+                "use": key["use"],
+                "n": key["n"],
+                "e": key["e"]
+            }
+    if rsa_key:
+        try:
+            payload = jwt.decode(
+                jwt_token,
+                rsa_key,
+                algorithms=ALGORITHMS,
+                audience=API_IDENTIFIER,
+                issuer=AUTH0_DOMAIN+"/"
+            )
+        except jwt.ExpiredSignatureError:
+            raise AuthError({"code": "token_expired",
+                            "description": "token is expired"}, 401)
+        except jwt.JWTClaimsError as e:
+            print(e)
+            raise AuthError({"code": "invalid_claims",
+                            "description":
+                                "incorrect claims,"
+                                " please check the audience and issuer"}, 401)
+        except Exception:
+            raise AuthError({"code": "invalid_header",
+                            "description":
+                                "Unable to parse authentication"
+                                " token."}, 401)
+    return payload
 
 
 def get_user_info(sf, email):
@@ -93,15 +157,7 @@ def requires_auth(sf):
         @wraps(f)
         def decorated(*args, **kwargs):
             token = get_token_auth_header()
-            # request user info from auth0
-            headers = {'Authorization': "Bearer " + token}
-            r = requests.get(url = AUTH0_DOMAIN + "/userinfo", headers=headers)
-
-            if r.status_code != 200:
-                raise AuthError({"code": "invalid_token",
-                    "description": "Your Authorization token is invalid"}, 401)
-
-            auth_info = r.json()
+            auth_info = decode_and_verify_payload(token)
 
             # assuming that email is in the 'name' field, due to
             # how auth0 handles signup.
