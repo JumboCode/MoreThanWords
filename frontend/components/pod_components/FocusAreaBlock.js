dbb27066ec0c9b4f87f592b6bb99810b484bc8d2
Jacqueline Chin <chin.jacqueline1@gmail.com>
2021-03-12 22:38:05 -0500

    removed some unnecessary includes at the top

--- frontend/components/pod_components/FocusAreaBlock.js
+++ frontend/components/pod_components/FocusAreaBlock.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView } from 'react-native';
-import Constants from 'expo-constants';
+import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+
 
 import PodProgressBar from './PodProgressBar.js';
 
