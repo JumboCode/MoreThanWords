7b56b0a27774a1a895e0bc9fde524a0c6df03ce7
Irina Wang <irina.wang@outlook.com>
2021-03-28 21:18:54 -0400

    added stylings for ongoing changes

--- frontend/components/ProgressBar.js
+++ frontend/components/ProgressBar.js
@@ -22,7 +22,7 @@ const ProgressBar = (props) => {
   return (
     <View style={{alignItems: 'center'}}>
       <View style={styles.ProgressBar}>
-        <Animated.View style={[StyleSheet.absoluteFill], {backgroundColor: '#27B48F', width, borderRadius: 10, margin: 2}}/>
+        <Animated.View style={[StyleSheet.absoluteFill], {backgroundColor: '#27b48f', width, borderRadius: 10, margin: 2}}/>
       </View>
       <Text style = {styles.ProgressBarText}>
         {`${props.progress} of ${props.total_outcomes} outcomes achieved`}
@@ -34,16 +34,33 @@ const ProgressBar = (props) => {
 export default ProgressBar;
 
 const styles = StyleSheet.create({
+  ongoing_ProgressBar: {
+    flexDirection: 'row',
+    height: 20,
+    width: 250,
+    backgroundColor: '#27b48f',
+    borderColor: 'white',
+    borderWidth: 2,
+    borderRadius: 10
+  },
+  ongoing_ProgressBarText: {
+    color: '#FFFFFF'
+},
+
+// complete
   ProgressBar: {
     flexDirection: 'row',
     height: 20,
     width: 250,
     backgroundColor: 'white',
+    // backgroundColor: '#27b48f',
     borderColor: '#27b48f',
+    // borderColor: 'white',
     borderWidth: 2,
     borderRadius: 10
   },
   ProgressBarText: {
-    color: '#3F3F3F',
+    color: '#3F3F3F'
+    // color: '#FFFFFF'
 },
 });
