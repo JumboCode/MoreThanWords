33a50896d29afa2b4e7fb0dc1ba6edc256062c73
Abe Park <abepark26@gmail.com>
2021-04-01 08:47:52 -0400

    adding scroll view into homescreen

--- frontend/components/pod_components/HomeScreen.js
+++ frontend/components/pod_components/HomeScreen.js
@@ -1,25 +1,52 @@
 import Constants from 'expo-constants';
 import React from 'react';
-import { SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity } from 'react-native';
-import { getAccessToken } from '../../utils/auth.js';
+import { SafeAreaView, ScrollView, StyleSheet } from 'react-native';
 
 import HomeScreenPod from './HomeScreenPod.js'
-import ProgressBar from '../ProgressBar.js';
 
 const server_add = Constants.manifest.extra.apiUrl;
 const pods = ['Trainee', 'Associate', 'Partner']; // hardcoded pod names for data query and subsequent screens
 
 export default class HomeScreen extends React.Component{
-    render(){
+    render() {
+        let IDKey = 0;
         return(
-            pods.map((pod,index) => 
-                <HomeScreenPod 
-                    key={index} // guarantee a unique id for each item
-                    pod={pod} 
-                    route={this.props.route}
-                    navigation={this.props.navigation} 
-                />)
-        )
+            <ScrollView>
+                <SafeAreaView style={styles.container}>
+                    {Object.entries(this.state.pods).map(([key, value]) => {
+                        IDKey++;
+                            return (
+                                <HomeScreenPod
+                                    pod={key}
+                                    key={IDKey}
+                                    route={this.props.route}
+                                    navigation={this.props.navigation}
+                                />
+                            )
+                        })}
+                </SafeAreaView>
+            </ScrollView>
+
+
+
+
+
+
+            // pods.map((pod,index) => 
+            //     <HomeScreenPod 
+            //         key={index} // guarantee a unique id for each item
+            //         pod={pod} 
+            //         route={this.props.route}
+            //         navigation={this.props.navigation} 
+            //     />
+            // )
+
+
+
+        );
+
+
+
     }
 }
 
