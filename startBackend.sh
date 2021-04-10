1b0556a9094df6bf5b711117fd931339ff5d8e8e
Ryan Beckwith <ryanbeckwith2000@gmail.com>
2021-04-10 11:10:24 -0400

    Untracked files in .gitignore

--- startBackend.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cd backend
-python3 -m venv backendEnv
-source backendEnv/bin/activate
-pip install -r requirements.txt
-eval $(egrep -v '^#' ../.env | xargs) python3 app.py
--- startBackendWindows.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cd backend
-py -m venv backendEnv
-source backendEnv/scripts/activate
-pip install -r requirements.txt
-eval $(egrep -v '^#' ../.env | xargs) py app.py
